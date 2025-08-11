bac758ff40de0d027360d05e86363417d6f4620c
Elcan <elcanmammadof@gmail.com>
2025-08-12 01:07:02 +0400

    updated README.md file

--- src/main/java/com/EmailSender/EmailSender/controller/EmailController.java
+++ src/main/java/com/EmailSender/EmailSender/controller/EmailController.java
@@ -3,7 +3,10 @@ package com.EmailSender.EmailSender.controller;
 import com.EmailSender.EmailSender.module.dto.request.EmailRequest;
 import com.EmailSender.EmailSender.service.EmailService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
